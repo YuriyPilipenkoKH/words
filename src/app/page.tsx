40b6ed5739368104c683b5f7ebd80574f34d1b61
Yuriy Pilipenko <yurik2061@gmail.com>
2026-04-23 22:34:46 +0300

    set home bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main className="flex-1 flex items-center justify-center">
-      <Image
-        src={process.env.NEXT_PUBLIC_HOME_BG || "/home-bg.png"}
-        alt="Home Background"
-        width={800}
-        height={600}
-        className="object-cover rounded-lg shadow-lg"
-      />
+    <main className="min-h-screen  bg-cover bg-center"
+      style={{
+       backgroundImage: `url(${process.env.NEXT_PUBLIC_HOME_BG})`,
+      }}
+    >
+      <h1>words app</h1>
     </main>
   );
 }
