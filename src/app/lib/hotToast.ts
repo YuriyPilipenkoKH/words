d94fc02d4ffedd10bb901c57d0b00a5efaa02d51
Yuriy Pilipenko <yurik2061@gmail.com>
2026-04-25 04:53:13 +0300

    toaser settings

---
+++ src/app/lib/hotToast.ts
@@ -0,0 +1,18 @@
+export const options={
+  success: {
+    duration: 3000,
+    style: {
+      background: 'green',
+      border: '2px solid orange',
+      color: '#eee',
+    },
+  },
+  error: {
+    duration: 3000,  
+    style: {
+      background: 'crimson',
+      border: '2px solid black',
+      color: '#eee',
+    },
+  },
+}
